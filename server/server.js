d468c52a091fc01f2eae8445c3679ce4e2411064
Christian <christian.a.faber@gmail.com>
2024-03-17 09:35:42 -1000

    got that post

--- server/server.js
+++ server/server.js
@@ -1,35 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 // const { MongoClient } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const connectionString = process.env.MONGO_URI;
-const postRouter = require("./routers/postRouter");
 // const commentRouter = require("./routers/commentRouter");
 
+//middleware goes before using routes!!!!!!
 const app = express();
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(morgan("combined"));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 const PORT = 3000;
 
+const postRouter = require("./routers/postRouter");
 app.use("/posts", postRouter);
 // app.use("/comments", commentRouter);
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
 
 mongoose
   .connect(connectionString)
   .then(() => console.log("Connected to the database…"))
   .catch((err) => console.log("Connection error:", err));
-
-app.get("/", (req, res, next) => {
-  res.send("Hello from Express!");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
